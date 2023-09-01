2782aa13e251457d93baa0c50eb59f7fcf3175bd
Kangzlz <127727428+Kangzlz@users.noreply.github.com>
2023-09-01 20:45:03 +0800

    6-4

---
+++ List6-15.c
@@ -0,0 +1,14 @@
+/*程序名和程序形式参数的显示（其二：以字符串为单位遍历命令行参数）*/
+#include<stdio.h>
+int main(int argc,char **argv){
+    int i=0;
+    char c;
+    while(argc-->0){
+        printf("argv[%d]=\"",i++);
+        while(c=*(*argv)++)
+        putchar(c);
+        argv++;
+        printf("\"\n");
+    }
+    return 0;
+}
